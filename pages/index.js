d45c26023b2e830fbab3eccdea985947ad0b1fb2
mahsa <mahsa_karim16@yahoo.com>
2022-06-18 15:34:06 +0430

    project

--- pages/index.js
+++ pages/index.js
@@ -1,209 +1,114 @@
+import React, { useState, useEffect } from 'react'
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 
-export default function Home() {
+import { publicFetch } from '../util/fetcher'
+
+import Layout from '../components/layout'
+import QuestionWrapper from '../components/question/question-wrapper'
+import QuestionStats from '../components/question/question-stats'
+import QuestionSummary from '../components/question/question-summary'
+import PageTitle from '../components/page-title'
+import ButtonGroup from '../components/button-group'
+import { Spinner } from '../components/icons'
+
+const HomePage = () => {
+  const router = useRouter()
+
+  const [questions, setQuestions] = useState(null)
+  const [sortType, setSortType] = useState('Votes')
+
+  useEffect(() => {
+    const fetchQuestion = async () => {
+      const { data } = await publicFetch.get('/question')
+      setQuestions(data)
+    }
+
+    const fetchQuestionByTag = async () => {
+      const { data } = await publicFetch.get(`/questions/${router.query.tag}`)
+      setQuestions(data)
+    }
+
+    if (router.query.tag) {
+      fetchQuestionByTag()
+    } else {
+      fetchQuestion()
+    }
+  }, [router.query.tag])
+
+  const handleSorting = () => {
+    switch (sortType) {
+      case 'Votes':
+        return (a, b) => b.score - a.score
+      case 'Views':
+        return (a, b) => b.views - a.views
+      case 'Newest':
+        return (a, b) => new Date(b.created) - new Date(a.created)
+      case 'Oldest':
+        return (a, b) => new Date(a.created) - new Date(b.created)
+      default:
+        break
+    }
+  }
+
   return (
-    <div className="container">
+    <Layout>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>
+          {router.query.tag ? router.query.tag : 'Questions'} - Clone of
+          Stackoverflow
+        </title>
       </Head>
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <PageTitle title={router.query.tag ? `Questions tagged [${router.query.tag}]` : 'All Ideas'} button borderBottom={false} />
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+      <ButtonGroup
+        borderBottom
+        buttons={['Votes', 'Views', 'Newest', 'Oldest']}
+        selected={sortType}
+        setSelected={setSortType}
+      />
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      {!questions && (
+        <div className="loading">
+          <Spinner />
         </div>
-      </main>
+      )}
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
+      {questions
+        ?.sort(handleSorting())
+        .map(
+          ({
+            id,
+            votes,
+            answers,
+            views,
+            title,
+            text,
+            tags,
+            author,
+            created
+          }) => (
+            <QuestionWrapper key={id}>
+              <QuestionStats
+                voteCount={votes.length}
+                answerCount={answers.length}
+                view={views}
+              />
+              <QuestionSummary
+                id={id}
+                title={title}
+                tags={tags}
+                author={author}
+                createdTime={created}
+              >
+                {text}
+              </QuestionSummary>
+            </QuestionWrapper>
+          )
+        )}
+    </Layout>
   )
 }
+
+export default HomePage
---
+++ site.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: 'IdeaCenter',
+  description: 'Where People Share Their Ideas',
+  siteUrl: 'https://idea-center.vercel.app',
+  googleAnalytic: 'UA-149699646-1',
+  author: {
+    email: 'mahsa_karim16@yahoo.com',
+    name: 'Mahsa'
+  },
+  lang: 'en'
+}
